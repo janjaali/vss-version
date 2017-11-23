9029723d8063cb5ddfa909593769d3c43e586093
Siyavash Habashi <ghashange@live.com>
2017-11-23 22:41:36 +0100

    linting

--- tests/lib/VersionTest.ts
+++ tests/lib/VersionTest.ts
@@ -2,9 +2,7 @@ import Version from '../../src/lib/Version';
 
 describe('Version', () => {
     it('constructor enforces semver version strings', () => {
-        /* tslint:disable */
-        expect(() => { new Version('1.x.2.a') }).toThrowError();
-        /* tslint:enable */
+        expect(() => (new Version('1.x.2.a'))).toThrowError();
     });
 
     describe('increment versions', () => {
