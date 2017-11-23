114ff0850fa28aec8b8a8cc356a5a0f247ed32ae
Siyavash Habashi <ghashange@live.com>
2017-11-23 22:44:13 +0100

    refactor tests

--- tests/lib/VssExtensionConfigParserTest.ts
+++ tests/lib/VssExtensionConfigParserTest.ts
@@ -2,15 +2,15 @@ import Version from '../../src/lib/Version';
 import VssExtensionConfigParser from '../../src/lib/VssExtensionConfigParser';
 
 describe('VssExtensionConfigParser', () => {
+    const vssExtConfParser = new VssExtensionConfigParser();
+
     it('parse version as string from vss-extension.json file', async () => {
-        const vssExtConfParser = new VssExtensionConfigParser();
         const version = await vssExtConfParser.parseVersion();
         const expectedVersion = new Version('1.0.7.5');
         expect(version).toEqual(expectedVersion);
     });
 
     it('throws error if version is not set in vss-extension.json file', async () => {
-        const vssExtConfParser = new VssExtensionConfigParser();
         await expect(vssExtConfParser).rejects;
     });
 });
